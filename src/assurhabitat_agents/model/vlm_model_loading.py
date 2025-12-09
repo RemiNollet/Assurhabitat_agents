238786325a72a36d7ad7327481633fda80cc1e43
RemiNollet <remi.nollet@live.fr>
2025-12-09 08:00:39 +0000

    change model for Qwen2.5 due to  permission not granted by llama

--- src/assurhabitat_agents/model/vlm_model_loading.py
+++ src/assurhabitat_agents/model/vlm_model_loading.py
@@ -1,4 +1,4 @@
-#src/assurhabitat_agents/model/vlm_model_loading.py
+"""#src/assurhabitat_agents/model/vlm_model_loading.py
 import requests
 import torch
 
@@ -6,16 +6,14 @@ from PIL import Image
 from transformers import MllamaForConditionalGeneration, AutoProcessor
 from huggingface_hub import login
 
-from assurhabitat_agents.config.LLM_config import HF_TOKEN, BASE_MODEL, MAX_NEW_TOKENS
+from assurhabitat_agents.config.model_config import HF_TOKEN, VLM_BASE_MODEL
 
 login(token=HF_TOKEN)
 
-model_id = "meta-llama/Llama-3.2-11B-Vision-Instruct"
-
 model = MllamaForConditionalGeneration.from_pretrained(
-    model_id,
+    VLM_BASE_MODEL,
     torch_dtype=torch.bfloat16,
-    device_map="auto",
+    device_map="auto"
 )
 processor = AutoProcessor.from_pretrained(model_id)
 
@@ -38,3 +36,36 @@ def vlm_inference(image, text="Just analyze the picture."):
     output = model.generate(**inputs, max_new_tokens=30)
     
     return processor.decode(output[0])
+"""
+
+from transformers import AutoProcessor, AutoModelForVision2Seq
+import torch
+from PIL import Image
+from huggingface_hub import login
+
+from assurhabitat_agents.config.model_config import HF_TOKEN, VLM_BASE_MODEL
+
+login(token=HF_TOKEN)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+processor = AutoProcessor.from_pretrained(VLM_BASE_MODEL)
+model = AutoModelForVision2Seq.from_pretrained(
+    VLM_BASE_MODEL,
+    dtype=torch.float16,
+    device_map="auto"
+)
+
+def vlm_inference(image, text):
+    messages = [
+        {"role": "user", "content": [
+            {"type": "image"},
+            {"type": "text", "text": text}
+        ]}
+    ]
+    
+    prompt = processor.apply_chat_template(messages, add_generation_prompt=True)
+    inputs = processor(prompt, image, return_tensors="pt").to(device)
+    generated_ids = model.generate(**inputs, max_new_tokens=30)
+    output = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return output
